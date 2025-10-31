6eeb6e2a1b7947fdb81e5b1ad31d55a75666d882
Candido <thiagocandido.bending@gmail.com>
2025-10-31 14:05:55 -0300

    Moviedux React App

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,36 +1,113 @@
-import React, {useState, useEffect} from 'react';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function MoviesGrid(){
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
 
-    const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
 
-    
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
 
-    useEffect(() => {
-        fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data))
-
-    }, []);
-
+  const matchesGenre = (movie, genre) => {
     return (
-        <div className='movies-grid'>
-
-            {
-                movies.map(movie => (
-                    <div key={movie.id} className='movie-card'>
-                        <img src={`images/${movie.image}`} alt={movie.title}/>
-                        <div className='movie-card-info'> 
-                            <h3 className='movie-card-title'>{movie.title}</h3>
-                            <p className='movie-card-genre'>{movie.genre}</p>
-                            <p className='movie-card-rating'>{movie.rating}</p>
-                        </div>
-                    </div>
-                ))
-            };
-
-        </div>
-    
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
     );
+  };
+
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+
+      case "Good":
+        return movie.rating >= 8;
+
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 8;
+
+      case "Bad":
+        return movie.rating < 5;
+
+      default:
+        return false;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movies) =>
+      matchesGenre(movies, genre) &&
+      matchesRating(movies, rating) &&
+      matchesSearchTerm(movies, searchTerm)
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+        ;
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Watchlist.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+
+export default function Watchlist() {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+    </div>
+  );
+}
