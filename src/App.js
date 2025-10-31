2565d8c9ec222a40a5912dd3a8a48a9a6053a436
Candido <thiagocandido.bending@gmail.com>
2025-10-31 09:11:19 -0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Moviedux</h1>
-      </header>
-      <footer className='footer'>
-        <p className='footer'>Footer Content goes here</p>
-      </footer>
+      <div className='container'>
+        <Header></Header>
+      </div>
+
+
+      
+        <Footer></Footer>
     </div>
   );
 }
