45f7e8e9bb60fb1066e825b96e8d3feae63409de
Candido <thiagocandido.bending@gmail.com>
2025-10-31 11:39:34 -0300

    adding movies grid

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
+      
       <div className='container'>
         <Header></Header>
+        <MoviesGrid></MoviesGrid>
       </div>
-
-
-      
+        
         <Footer></Footer>
     </div>
   );
